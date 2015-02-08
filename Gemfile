4b5c930a16eda00fe1d280ac441f5c1e432b4880
Theresa Luu <theresa.luu@gmail.com>
2015-02-08 15:02:56 -0800

    added require bundler/setup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-ruby '2.0.0-p247'
+ruby '2.0.0'
 
 source 'https://rubygems.org'
 gem 'sinatra'
--- adventure.rb
+++ adventure.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'haml'
+require "bundler/setup"
 
 get '/' do
   haml :welcome

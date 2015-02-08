cf295f43d8398e84f40da923404625601c89b21f
Theresa Luu <theresa.luu@gmail.com>
2015-02-07 18:47:53 -0800

    sinatra app

---
+++ adventure.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'haml'
+
+get '/' do
+  haml :welcome
+end
+
+get '/nsx' do
+  haml :nsx
+end
+
+get '/nsx/chase-dog' do
+  haml :chase
+end
+
+get '/nsx/lure-with-donuts' do
+  haml :lure
+end
+
+get '/phillipe' do
+  haml :phillipe
+end
+
+get '/phillipe/ride' do
+  haml :ride
+end
+
+get '/phillipe/convince' do
+  haml :convince
+end
